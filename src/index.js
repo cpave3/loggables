e410c7f11fed7f5c47f1788ee733beb9fd11e802
Cameron Pavey <cpave3@gmail.com>
2018-05-29 22:42:05 +1000

    :construction: Work in progress websocket server
      - ADD: basic WS functionality
      - MOD: output mode for events

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ const connection = new(require('nosqlite').Connection)();
 const path = require('path');
 const Tail = require('tail-forever');
 const log = require('./consoleLog').log;
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const port = process.env.PORT || 4001;
+const index = require('./routes/index');
+const app = express();
+app.use(index);
+const server = http.createServer(app);
+const io = socketIo(server);
 
 // -----
 
 const tailTarget = __dirname+'/../sample-files/sample.log1';
+
+const clients = [];
 
 function exitApp() {
     log(`Exiting...`, 'danger');
@@ -30,6 +42,23 @@ db.exists((exists) => {
     }
 });
 
+io.on('connection', (socket) => {
+    rememberSocket(socket);
+    const data = db.allSync();
+    console.log(data);
+    io.emit('allData', data)
+});
+
+function rememberSocket(socket) {
+    // Add the new connection to the list of clients
+    clients.push(socket);
+    log('New client connected', 'good');
+    io.on('disconnect', () => {
+        clients.splice(clients.indexOf(socket), 1);
+        log('Client disconnected', 'warning');
+    });
+}
+
 function appLoop() {
     tail = new Tail(tailTarget);
     tail.on("line", function(line) {
@@ -37,6 +66,9 @@ function appLoop() {
       db.post(logLine, (err, id) => {
         if (err) throw err;
         log(`Record ${id} inserted`);
+        const record = db.findSync({ id });
+        console.log(record);
+        io.emit('newLine', record);
       });
     });
     tail.on("error", function(error) {
@@ -44,5 +76,5 @@ function appLoop() {
     });
 }
 
-
+server.listen(port, () => console.log(`Listening on port ${port}`));
 
